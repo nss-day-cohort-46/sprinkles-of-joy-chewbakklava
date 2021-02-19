437b34a8fe1604ec911df20611691407ea609863
kjk1325e <kaitlinjkelley@gmail.comm>
2021-02-19 15:41:12 -0600

    working on display change on click

--- scripts/contact/ContactForm.js
+++ scripts/contact/ContactForm.js
@@ -1,15 +1,32 @@
-let formContainer = document.querySelector("#contactForm")
+let formContainer = document.querySelector(".contact")
 
 export const contactForm = () => {
+    
     formContainer.innerHTML = `
-        <h2>Contact Form</h2>
-        <label for="contactForm__email">Email</label>
-        <input id="contactForm__email" type="email" name="email">
+        <div id="modal--contact" class="modal--parent">
+            <div class="modal--content">
+                <form>
+                <h2>Contact Form</h2>
+                <label for="contactForm__email">Email</label>
+                <input id="contactForm__email" type="email" name="email">
 
-        <label for="contactForm__phone">Phone(Do not include special characters, such as dashes or parentheses)</label>
-        <input id="contactForm__phone" type="text" name="phoneNum">
-        
-        <label for="contactForm__message">Message</label>
-        <input id="contactForm__message" type="textarea" name="message">
+                <label for="contactForm__phone">Phone(Do not include special characters, such as dashes or parentheses)</label>
+                <input id="contactForm__phone" type="text" name="phoneNum">
+                
+                <label for="contactForm__message">Message</label>
+                <input id="contactForm__message" type="textarea" name="message">
+
+                <input type="submit" value="submit">
+                </form>
+            </div>
+        </div>
     `
 }
+
+const eventHub = document.querySelector("#container")
+
+eventHub.addEventListener("click", event => {
+    if (event.target.id === "showContactForm") {
+        formContainer.style.display = "flex"
+    }
+})
