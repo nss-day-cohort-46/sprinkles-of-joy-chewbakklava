c16d47d1563f8150a8c5c15450bc2a16ee7ca927
Alex Martin <alex@subtleco.co>
2021-02-19 08:51:19 -0600

    Review form opens and closes

--- scripts/reviews/ReviewForm.js
+++ scripts/reviews/ReviewForm.js
@@ -10,9 +10,28 @@ eventHub.addEventListener("reviewFormRequested", e => {
 
 export const ReviewForm = productId => {
     if (authHelper.isUserLoggedIn()) {
+        const reviewFormHTML = `
+        
+        <div id="review__modal" class="modal--parent">
+            <div class="modal--content">
+                <form class="reviewForm">
+                    <span class="close">&times;</span>
+                    <input type="textarea" id="reviewForm__text">
+                    <select name="reviewForm__rating" id="reviewForm__rating">
+                        <option value="1">⭐️</option>
+                        <option value="2">⭐️⭐️</option>
+                        <option value="3">⭐️⭐️⭐️</option>
+                        <option value="4">⭐️⭐️⭐️⭐️</option>
+                        <option value="5">⭐️⭐️⭐️⭐️⭐️</option>
+                    </select>
+                </form>
+            </div>
+        </div>
+        `
 
+        reviewModalElement.innerHTML = reviewFormHTML
+        reviewModalElement.classList.remove("hidden")
     } else {
         alert(`Please login or create an account to leave a review!`)
     }
-
 }
